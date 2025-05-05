664fe870e8188f591cc9837a73eb627c853f6854
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-05 12:18:32 +0100

    detection si existe erreur lors de lancement de app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
-
+  .then(() => {
+    console.log('L\'application a démarré avec succès.');
+  })
+  .catch((err: any) => {
+    console.error('Erreur lors du démarrage de l\'application:', err);
+  });
