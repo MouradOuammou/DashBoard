9f1fcb70b185325f6aef119f826d3b423866cd9a
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-05 16:41:05 +0100

    add view user et logique

--- src/app/views/Users/users.component.ts
+++ src/app/views/Users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -6,6 +8,47 @@ import { Component } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: User[] = [];
+  loading = true;
+  error: string | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchUsers();
+  }
+
+  fetchUsers(): void {
+    this.loading = true;
+    this.error = null;
+
+    this.http.get<User[]>('http://localhost:3000/api/users')
+      .subscribe({
+        next: (data) => {
+          this.users = data;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.error = 'Erreur lors du chargement des utilisateurs';
+          this.loading = false;
+          console.error('Error fetching users:', err);
+        }
+      });
+  }
+
+  // Méthodes pour les actions
+  viewUserDetails(user: User): void {
+    console.log('View user:', user);
+  }
+
+  editUser(user: User): void {
+    console.log('Edit user:', user);
+  }
+
+  deleteUser(user: User): void {
+    if (confirm(`Êtes-vous sûr de vouloir supprimer ${user.prenom} ${user.nom} ?`)) {
+      console.log('Delete user:', user);
+    }
+  }
 }
